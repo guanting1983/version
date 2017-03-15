9781455da98ec111e3725cda4a5c1f7d4b6349f2
Lenovo <Lenovo@Lenovo-PC>
2017-03-15 13:15:14 +0800

    describe

---
+++ version/src/com/qianfeng/test/MainTest.java
@@ -0,0 +1,21 @@
+package com.qianfeng.test;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		int age;
+		
+		sum();
+		
+		
+	}
+
+	private static void sum() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+	
+}
